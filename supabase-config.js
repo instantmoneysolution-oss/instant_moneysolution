b9a1b3594d008013037c4e8d448bda06045ef525
Rohit Mani Tripathi <rohitmanitripathi2005@gmail.com>
2026-08-12 00:07:19 +0530

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://eypzqwfnuurimrucpjnc.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_CbkK0HrU_hbe9uMpsPOuwA_6GMGHfK6";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
