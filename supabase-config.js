85f3be97585a3bfba0a96098b58601531453f122
Rohit Mani Tripathi <rohitmanitripathi2005@gmail.com>
2026-09-16 10:51:29 +0530

    commit

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://eypzqwfnuurimrucpjnc.supabase.co";
 
-const SUPABASE_KEY = "sb_publishable_CbkK0Hr_U_hbe9uMpsPOuwA_6GMGHfK6";
+const SUPABASE_KEY = "sb_publishable_w5Ud8HipCytSC2_-mfUpbg_jPv4i4FM";
 
 window.supabaseClient = supabase.createClient(
     SUPABASE_URL,
