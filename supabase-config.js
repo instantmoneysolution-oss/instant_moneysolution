cf2dd008328542cb36dd066644fc0d7e249d3391
Rohit Mani Tripathi <rohitmanitripathi2005@gmail.com>
2026-09-16 12:07:46 +0530

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,8 +0,0 @@
-const SUPABASE_URL = "https://eypzqwfnuurimrucpjnc.supabase.co";
-
-const SUPABASE_KEY = "sb_publishable_w5Ud8HipCytSC2_-mfUpbg_jPv4i4FM";
-
-window.supabaseClient = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
