4d6990b011dde7b1f09ed4cc9c324197458b07db
Rohit Mani Tripathi <rohitmanitripathi2005@gmail.com>
2026-09-16 13:29:15 +0530

    commit

--- database.js
+++ database.js
@@ -1,25 +1,8 @@
-// ==========================================
-// SUPABASE CONFIGURATION
-// ==========================================
+const SUPABASE_URL = "https://tqgxdnlhmticsmoaeoqq.supabase.co";
 
-const SUPABASE_URL = "https://eypzqwfnuurimrucpjnc.supabase.co";
+const SUPABASE_KEY = "sb_publishable_w5Ud8HipCytSC2_-mfUpbg_jPv4i4FM";
 
-const SUPABASE_KEY =
-  "sb_publishable_CbkK0Hr_U_hbe9uMpsPOuwA_6GMGHfK6";
-
-
-// Check that Supabase library is loaded
-if (typeof supabase === "undefined") {
-  console.error(
-    "Supabase library is not loaded. Check the Supabase CDN script in your HTML."
-  );
-} else {
-
-  // Create Supabase client
-  window.supabaseClient = supabase.createClient(
+window.supabaseClient = supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
-  );
-
-  console.log("Supabase client initialized.");
-}
+);
