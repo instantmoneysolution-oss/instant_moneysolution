0f0ab18c1c64eb0a71a6e232aab7a2a2880ecf10
Rohit Mani Tripathi <rohitmanitripathi2005@gmail.com>
2026-09-16 12:08:26 +0530

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://tqgxdnlhmticsmoaeoqq.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_w5Ud8HipCytSC2_-mfUpbg_jPv4i4FM";
+
+window.supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
